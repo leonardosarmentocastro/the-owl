b66ddf27dd1f8c6b1cd80667b2d78a85405e595f
Leonardo Sarmento de Castro <leonardo.sarmentocastro@gmail.com>
2019-05-04 22:56:28 +0200

    Bugfix: optionally exports fixtures on shipped code.

--- src/redux/ducks/docs/index.js
+++ src/redux/ducks/docs/index.js
@@ -1,6 +1,10 @@
+// NOTE: Fixtures are not shipped on distribution code.
+const optional = require('optional');
+const fixtures = optional('./__fixtures__') || {};
+
 module.exports = {
   // CONVENTION: reducer exports functions with same name as actions, so export reducer first.
   ...require('./reducer'),
   ...require('./actions'),
-  ...require('./__fixtures__'),
+  ...fixtures,
 };
